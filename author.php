8a9c22ae588849f663086be137ac085a2467f699
Joe Lambert <joe@joelambert.co.uk>
2017-12-16 14:31:33 +0000

    Tidy up comments

--- author.php
+++ author.php
@@ -1,12 +1,6 @@
 <?php
 /**
  * The template for displaying Author Archive pages
- *
- * Methods for TimberHelper can be found in the /lib sub-directory
- *
- * @package  WordPress
- * @subpackage  Timber
- * @since    Timber 0.1
  */
 
 namespace App;
