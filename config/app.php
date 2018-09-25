decc46e064c05b02ef7e7a18617b86acaf2f5481
Adam Tomat <adam.tomat@rareloop.com>
2018-09-25 19:53:02 +0100

    Revert v4 changes

--- config/app.php
+++ config/app.php
@@ -23,9 +23,6 @@ return [
         Rareloop\Lumberjack\Providers\MenusServiceProvider::class,
         Rareloop\Lumberjack\Providers\LogServiceProvider::class,
         Rareloop\Lumberjack\Providers\ThemeSupportServiceProvider::class,
-        Rareloop\Lumberjack\Providers\QueryBuilderServiceProvider::class,
-        Rareloop\Lumberjack\Providers\SessionServiceProvider::class,
-        Rareloop\Lumberjack\Providers\EncryptionServiceProvider::class,
     ],
 
     /**
